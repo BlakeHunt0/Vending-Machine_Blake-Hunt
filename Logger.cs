eda93619976d9a716a82b465082cf2998e90fa5f
Blake <nevaeh.hunt@student.centralia.edu>
2025-02-20 16:45:50 -0800

    Add project files.

---
+++ Logger.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Vending_Machine_Blake_Hunt
+{
+    internal class Logger
+    {
+        public static void logToText(string s)
+        {
+            //txt location
+            string logTxt = @"C:\Users\Brine\OneDrive\Desktop\Vending_Machine_Blake_Hunt_2025-master\Vending_Machine_Blake_Hunt_2025-master\VendLog.txt";
+
+            //add text to the txt append style
+            StreamWriter log = File.AppendText(logTxt);
+
+            string logAppend = s;
+
+            log.WriteLine(logAppend);
+            log.Close();
+        }
+    }
+}
